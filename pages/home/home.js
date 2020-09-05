0452325517b10189caffe4e1cba2233b3807b513
Your Name <2242817829@qq.com>
2020-09-05 18:27:43 +0800

    01-体验小程序

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: 'coderwhh',
+    age: 12,
+    students: [
+      {id: 1, name: "猪八戒", age: 10},
+      {id: 2, name: "孙悟空", age: 11},
+      {id: 3, name: "沙和尚", age: 12}
+    ],
+    counter: 0
   },
 
+  addCounter() {
+    // 1.错误: 界面是不会刷新的
+    // this.data.counter++  
+
+    // 2.this.setData
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+
+  subcounter() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
